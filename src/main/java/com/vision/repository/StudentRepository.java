6040cbfbfae246a0f5fa1cdf5a217195e826dab4
Jagdish3396 <bornarejagdish7@gmail.com>
2023-10-21 23:01:49 +0530

    commit

---
+++ src/main/java/com/vision/repository/StudentRepository.java
@@ -0,0 +1,11 @@
+package com.vision.repository;
+
+//import org.springframework.data.repository.CrudRepository;
+//
+//import com.vision.entity.StudentEntity;
+
+public interface StudentRepository {
+	//extends CrudRepository<StudentEntity, Integer>{
+//}
+
+}
---
+++ src/main/java/com/vision/repository/TimingRepo.java
@@ -0,0 +1,10 @@
+package com.vision.repository;
+
+//import org.springframework.data.repository.CrudRepository;
+//
+//import com.vision.entity.Timings;
+
+public interface TimingRepo {
+//extends CrudRepository<Timings, Integer> {
+
+}
